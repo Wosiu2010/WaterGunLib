367a60e740ed4dcab7a550b53b0bb05524eb75b2
WaterGun <130384497+Wosiu2010@users.noreply.github.com>
2026-04-04 17:19:20 +0200

    Added GrassShaderRestorer

---
+++ GrassShaderRestorer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WaterGunLib
+{
+    public class GrassShaderRestorer : MonoBehaviour
+    {
+        public List<Material> materialsToApplyShader = new List<Material>();
+        public bool ApplyShaderOnAwake = true;
+        public void Awake()
+        {
+            if (ApplyShaderOnAwake)
+            {
+                Shader grassWaving = GetGrassShader();
+
+
+                if (materialsToApplyShader.Count == 0) return;
+                foreach (var mat in materialsToApplyShader)
+                {
+
+                    if (grassWaving == null)
+                        Debug.LogError("Shader missing");
+                    else
+                        mat.shader = grassWaving;
+                }
+            }
+        }
+
+        public Shader GetGrassShader()
+        {
+            return Shader.Find("Shader Graphs/WavingGrass");
+        }
+    }
+}
